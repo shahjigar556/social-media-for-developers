7a2efc0c0605a80a3b4156a9037e52961cedd96b
Jigar <shahjigar185@gmail.com>
2021-02-09 01:34:19 +0530

    Added middle ware for accessing protected route

---
+++ middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt=require('jsonwebtoken')
+const config=require('config')
+
+module.exports=function(req,res,next){
+    try{
+      const token=req.header('x-auth-token')
+      if(!token){
+          return res.status(401).json({"msg":"Access Denied"})
+      }
+      const decoded=jwt.verify(token,config.get('jwtKey'))  // decoding the payload
+      req.user=decoded
+      next();
+    }catch(ex){
+        return res.status(404).json({"msg":"Authentication failed(wrong token)"})
+    }
+}
