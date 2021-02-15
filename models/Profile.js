96aac599e207bc23bc05c1604694ae79e6941ab8
Jigar <shahjigar185@gmail.com>
2021-02-15 23:47:51 +0530

    Developed the profile route till Profile experience

---
+++ models/Profile.js
@@ -0,0 +1,149 @@
+const mongoose=require('mongoose');
+const Joi=require('joi');
+const ProfileSchema=mongoose.Schema({
+  user:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'User'
+  },  // linking the user data in profile
+  company:{
+      type:String
+  },
+  website:{
+      type:String
+  },
+  location:{
+      type:String
+  },
+  status:{
+      type:String,
+      required:true
+  },
+  skills:{
+      type:[String],  // array of Strings for skills
+      required:true,
+  },
+  bio:{
+      type:String
+  },
+  githubusername:{
+     type:String
+  },
+  experience:[
+      {
+          title:{
+              type:String,
+              required:true
+          },
+          company:{
+              type:String,
+              required:true,
+          },
+          location:{
+              type:String
+          },
+          from:{
+              type:String,
+              required:true
+          },
+          to:{
+              type:String,
+          },
+          current:{
+              type:Boolean,
+              default:false
+          },
+          description:{
+              type:String
+          }
+      }
+  ],
+  education:[
+      {
+          school:{
+              type:String,
+              required:true
+          },
+          degree:{
+              type:String
+          },
+          fieldofstudy: {
+            type: String,
+            required: true
+          },
+          from: {
+            type: String,
+            required: true
+          },
+          to: {
+            type: String
+          },
+          current: {
+            type: Boolean,
+            default: false
+          },
+          description: {
+            type: String
+          }
+      }
+  ],
+  social:{
+      youtube:{
+          type:String
+      },
+      twitter: {
+        type: String
+      },
+      facebook: {
+        type: String
+      },
+      linkedin: {
+        type: String
+      },
+      instagram: {
+        type: String
+      }
+  },
+  date:{
+      type:Date,
+      default:Date.now
+  }
+
+})
+
+function validateProfile(user){
+    const schema=Joi.object({
+        skills:Joi.string().required(),
+        status:Joi.string().required(),
+        company:Joi.string(),
+        location:Joi.string(),
+        website:Joi.string(),
+        bio:Joi.string(),
+        githubusername:Joi.string(),
+        youtube:Joi.string(),
+        twitter:Joi.string(),
+        facebook:Joi.string(),
+        linkedin:Joi.string(),
+        instagram:Joi.string(),
+    })
+    return schema.validate(user);
+}
+
+function validateExperience(exp){
+    console.log(exp);
+    const schema=Joi.object({
+        title:Joi.string().required(),
+        company:Joi.string().required(),
+        from :Joi.string().required(),
+        to:Joi.string(),
+        current:Joi.boolean(),
+        location:Joi.string(),
+        description:Joi.string()
+    })
+    console.log(schema.validate(exp))
+    return schema.validate(exp)
+}
+const Profile=mongoose.model('profile',ProfileSchema);
+
+module.exports.Profile=Profile;
+module.exports.validateProfile=validateProfile;
+module.exports.validateExperience=validateExperience;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,236 @@
-const app=require('express')
-const router=app.Router();
-const auth=require('../../middleware/auth')
-// @GET api/profile
-// @desc
-//@Private
+const app = require("express");
+const router = app.Router();
+const auth = require("../../middleware/auth");
+const { Profile, validateProfile,validateExperience } = require("../../models/Profile");
+const { Users } = require("../../models/Users");
 
-router.get('/',auth,async (req,res)=>{
+// @GET api/profile/me
+// @desc  accesing the profile of the user
+// @Private
 
-    res.send("Profile Route")
-    console.log(req.user)
+router.get("/me", auth, async (req, res) => {
+  // user is authenticated
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+  
+    if (!profile) {
+      return res.status(400).json({ msg: "No Profile Present" });
+    }
+    res.json(profile);
+  } catch (ex) {
+    console.log(ex.message);
+    return res.status(500).send("server error");
+  }
+});
+
+//   @POST api/profile
+//   @desc creating or updating the profile
+//   @Private
+
+router.post("/", auth, async (req, res) => {
+  //validate data
+  const {error}=validateProfile(req.body);
+  console.log(error)
+  if(error){
+      const message=error.details[0].message;
+      return res.status(400).json(message)
+  }
+
+  const {
+    company,
+    website,
+    location,
+    status,
+    bio,
+    githubusername,
+    youtube,
+    twitter,
+    facebook,
+    linkedin,
+    instagram,
+    skills,
+  } = req.body;
+
+  const ProfileFields = {};
+  ProfileFields.user = req.user.id;
+  if (company) ProfileFields.company = company;
+  if (website) ProfileFields.website = website;
+  if (location) ProfileFields.location = location;
+  if (status) ProfileFields.status = status;
+  if (bio) ProfileFields.bio = bio;
+  if (githubusername) ProfileFields.githubusername = githubusername;
+
+  // covert skills to array
+  if (skills) {
+    ProfileFields.skills = skills.split(",").map((skill) => skill.trim());
+  }
+  ProfileFields.social = {};
+  if (youtube) ProfileFields.social.youtube = youtube;
+  if (twitter) ProfileFields.social.twitter = twitter;
+  if (facebook) ProfileFields.social.facebook = facebook;
+  if (linkedin) ProfileFields.social.linkedin = linkedin;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+    if (profile) {
+      // update the profile
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set:  ProfileFields  },
+        { new: true }
+      );
+      return res.json(profile);
+    }
+
+    // create the profile
+    profile = new Profile(ProfileFields);
+    profile = await profile.save();
+
+    return res.json(profile);
+  } catch (ex) {
+    console.log(ex.message);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+//   @GET api/profile
+//   @desc Getting all the profiles
+//   @Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    if (!profiles) {
+      return res.status(400).json({ msg: "No profiles available" });
+    }
+    res.json(profiles);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send("Server error");
+  }
+});
+
+//   @GET api/profile/user/:user_id
+//   @desc Getting all the profiles
+//   @Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const id = req.params.user_id;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).json({ msg: "No profile present" });
+    }
+    const profile = await Profile.findOne({ user: req.params.user_id });
+    if (!profile) {
+      return res.status(400).json({ msg: "No profile found" });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send("server error");
+  }
+});
+
+//   @DELETE api/profile
+//   @desc Deleting profile,user
+//   @Private
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    const id = req.user.id;
+    await Profile.findOneAndRemove({ user: id });
+    await Users.findOneAndRemove({ _id: id });
+    res.json({ msg: "User deleted" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).send("server error");
+  }
+});
+
+//   @PUT api/profile/experience
+//   @desc Adding experience of the user
+//   @Private
+
+router.put('/experience',auth,async (req,res)=>{
+
+  const id=req.user.id;
+
+  try {
+    
+    const profile=await Profile.findOne({user:id})
+    if(!profile){
+      return res.status(400).json({msg:"First Complete the profile"});
+    }
+    // profile exists
+    let {
+      title,
+      company,
+      from,
+      to,
+      current,
+      location,
+      description
+    }=req.body;
+    
+    // converting current to boolean
+    if(current==='true')
+        current=true
+    else
+        current=false
+
+    req.body.current=current;
+    // validating the data
+    const {error}=validateExperience(req.body);
+    console.log(error)
+    if(error){
+      const message = error.details[0].message;
+      return res.status(400).json({ error: message });
+    }
+
+    const newExp={
+       title,
+       company,
+       from,
+       to,
+       current,
+       location,
+       description
+    }
+
+    profile.experience.unshift(newExp);
+    await profile.save();
+    res.json(profile)
+
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("server error");
+  }
+
 })
-module.exports=router;
+
+//   @DELETE api/profile/experience/:exp_id
+//   @desc Deleting experience of the user
+//   @Private
+router.delete('/experience/:exp_id',auth ,async (req,res)=>{
+
+  try {
+    const profile=await Profile.findOne({user:req.user.id});
+    if(!profile){
+      return res.status(400).json({msg:"Please make a profile first"});
+    }
+    const exp_id=req.params.exp_id;
+    if (!exp_id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).json({ msg: "No experience Found!" });
+    }
+    profile.experience=profile.experience.filter(item=>item._id!=exp_id)
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+
+})
+
+module.exports = router;
