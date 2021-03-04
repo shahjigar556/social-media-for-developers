636b03b1811ad5cca3d5dd3f349e79b2b56469d2
Jigar <shahjigar185@gmail.com>
2021-03-05 01:26:11 +0530

    Added the dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import {Route,Switch} from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route excat path='/login' component={Login} />
+      </Switch>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
