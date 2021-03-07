003d6a7eeb2c48efe5313cc9571d5fcbc4d06df4
Jigar <shahjigar185@gmail.com>
2021-03-08 00:48:02 +0530

    Rootreducer

---
+++ client/src/Redux/reducer/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import alertReducer from '../alert/reducer';
+
+const rootReducer=combineReducers({
+  alert:alertReducer,
+});
+
+export default rootReducer;
