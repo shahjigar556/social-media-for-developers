316fc81cdb0fbe95210382dfa144303e72250ed6
Jigar <shahjigar185@gmail.com>
2021-03-25 12:11:50 +0530

    Added accordion for displaying githun repos

--- client/src/components/dashboard/profiles/Github.js
+++ client/src/components/dashboard/profiles/Github.js
@@ -4,6 +4,11 @@ import {makeStyles} from '@material-ui/core';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import './github.css'
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root:{
@@ -19,6 +24,10 @@ const useStyles = makeStyles((theme) => ({
        textDecoration:'none',
        color:'black',
        marginLeft:'10px'
+    },
+    accordion:{
+         marginTop:'10px',
+         backgroundColor:'#E0E0E0'
     }
   }));
 
@@ -26,13 +35,15 @@ const useStyles = makeStyles((theme) => ({
 function Repo({repo}){
    const classes=useStyles();
     return (
-        <ul>
-            <li>
-                <a href={repo.html_url} className={classes.links} target="_blank">
-                    {repo.full_name}
-                </a>
-            </li>
-        </ul>
+        <AccordionDetails style={{display:'block'}}>
+            <ul>
+                <li>
+                    <a href={repo.html_url} className={classes.links} target="_blank">
+                        {repo.full_name}
+                    </a>
+                </li>
+            </ul>
+        </AccordionDetails>
     )
 }
 function Github({username}) {
@@ -53,7 +64,16 @@ function Github({username}) {
             <a className={classes.links} href={github_url} target='_blank' style={{color:'#17a2b8'}}>
                 Visit Github Profile
             </a>
-             {repos.map(repo=><Repo repo={repo} name={username}/>)}
+            <Accordion className={classes.accordion}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+             >
+                 <Typography className={classes.heading}>Github Repositories</Typography>
+            </AccordionSummary>
+                {repos.map(repo=><Repo repo={repo} name={username}/>)}
+            </Accordion>
         </div>
     )
 }
