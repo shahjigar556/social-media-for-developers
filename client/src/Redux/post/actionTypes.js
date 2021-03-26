3eadce4747f4bbc4d0d94b8939d4e26d4c752f44
Jigar <shahjigar185@gmail.com>
2021-03-26 14:45:50 +0530

    Post feature

---
+++ client/src/Redux/post/actionTypes.js
@@ -0,0 +1,9 @@
+export const GET_ALL_POST="GET_ALL_POST";
+export const POST_ERR="POST_ERR";
+export const UPDATE_LIKE="UPDATE_LIKE";
+export const REMOVE_LIKE="REMOVE_LIKE";
+export const DELETE_POST="DELETE_POST";
+export const ADD_POST="ADD_POST";
+export const GET_POST="GET_POST";
+export const ADD_COMMENT="ADD_COMMENT";
+export const DELETE_COMMENT="DELETE_COMMENT";
